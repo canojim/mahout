5adcb51160c37a4b320e514b014c42dae1318f0a
Hu, Yiqun <yiqun.hu@gmail.com>
2014-09-16 10:34:43 +0800

    Add a generic class PairWritable to refactor all the pair writable types

---
+++ core/src/main/java/org/apache/mahout/cf/taste/hadoop/als/PairWritable.java
@@ -0,0 +1,84 @@
+package org.apache.mahout.cf.taste.hadoop.als;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import com.google.common.collect.ComparisonChain;
+
+public class PairWritable<T1 extends WritableComparable, T2 extends WritableComparable> implements WritableComparable<DoubleIntPairWritable> {
+
+	private T1 first;
+	private T2 second;
+		
+	public PairWritable(T1 value1, T2 value2) {
+		this.first = value1;
+		this.second = value2;
+	}	
+	
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		first.readFields(in);
+		second.readFields(in);		
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		first.write(out);
+		second.write(out);
+	}
+
+	@Override
+	public int compareTo(PairWritable o) {
+		return ComparisonChain.start().compare(first.get(), o.first.get())
+		        .compare(second.get(), o.second.get()).result();		
+	}
+
+	@Override
+	public int hashCode() {
+
+		// http://stackoverflow.com/questions/10034328/hashcode-for-objects-with-only-integers
+		
+		final int prime = 31;
+	    int result = 1;
+	    	    
+	    result = prime * result + first.hashCode();
+	    result = prime * result + second.hashCode();
+	    return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof PairWritable) {
+			PairWritable w = (PairWritable) obj;
+			return first.equals(first) && second.equals(w.second); 
+		}
+		return false;
+	}
+
+	public T1 getFirst() {
+		return first;
+	}
+
+	public void setFirst(T1 first) {
+		this.first = first;
+	}
+
+	public T2 getSecond() {
+		return second;
+	}
+
+	public void setSecond(T2 second) {
+		this.second = second;
+	}
+
+	@Override
+	public String toString() {
+		return first.toString() + "," + second.toString();
+	}
+
+
+	
+}
